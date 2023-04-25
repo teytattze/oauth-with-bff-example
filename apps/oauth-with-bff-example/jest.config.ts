ec1b0226c361aab4643438c6ba7809ca6afb523b
teytattze <tattzetey@outlook.com>
2023-04-25 20:39:48 +0100

    Initial commit

---
+++ apps/oauth-with-bff-example/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: "oauth-with-bff-example",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+  },
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/apps/oauth-with-bff-example",
+};
